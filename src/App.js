5a62d2d90ffbc3194113d50f2f1fe090b686cb81
Maxime Bonhomme <maxime@everpress.com>
2021-12-07 19:52:20 +0000

    project base start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Flex, Text } from '@chakra-ui/react';
 
 function App() {
+  const connectWallet = async () => {
+    // we will try to connect to a wallet here :)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Flex
+      alignItems='center'
+      justifyContent='center'
+      height='100vh'
+      bg='purple.900'
+    >
+        <Button onClick={connectWallet}>Connect wallet</Button>
+    </Flex>
   );
 }
 
